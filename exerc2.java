0b2a4991fd1b22901a47deeb21777458f6e5daa0
Vitória Safira de Oliveira <80469017+SafiraBenvindo@users.noreply.github.com>
2024-03-12 09:43:47 -0300

    Add files via upload

---
+++ exerc2.java
@@ -0,0 +1,10 @@
+package Proj2;
+public class exerc2 {
+	public static void main(String[] args) {
+      String nome = "Garfield";
+      int idade = 10;
+      String Cor = "laranja";
+      System.out.printf("Nome: %s%nIdade: %d Cor: %d%n" , nome, idade, Cor);
+       
+	  }
+}
